0d3adebb042bd701361e16fcd64d0559bd7488c0
LAPTOP-GSD0DSCE\Sakura <Daima69.st@gmail.com>
2021-12-07 20:51:30 +0900

    見た目整える

--- tomori/index.php
+++ tomori/index.php
@@ -4,14 +4,16 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="../assets/css/reset.css">
+    <link rel="stylesheet" href="../assets/css/tomori.css">
   <title>Document</title>
 </head>
 <body>
-<<<<<<< HEAD
-  
- 
-=======
->>>>>>> 6952906219b88419d337c1c7276a1286f0f31711
-  
+<footer>
+    <p>Copyright <small>&copy;</small> JUNJI ERINA TOMORI Inc.</p>
+  </footer>
+</body>
+
+</html>
 </body>
 </html>
