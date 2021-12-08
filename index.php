6c651a7e438afeee2821e1023fc93fa9782eca66
DESKTOP-NET3VUU\mm_an <eri_matsuo@icloud.com>
2021-12-08 19:44:45 +0900

    全体ｈｔｍｌに貼った

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="ja">
 <head>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -7,6 +7,61 @@
   <title>Document</title>
 </head>
 <body>
-  
+  <!-- Erina blog -->
+<div class="content">
+<h2>Latest Article</h2>
+<ul class="container">
+    <li class="item">
+        <img src="assets/img/post_img_1.png" alt=""><br>
+        <time>2018-05-20</time>
+        <p>おしゃれカフェがありますよ</p>
+        <div class="read_more">READ MORE
+            <p></p>
+        </div>
+    </li>
+    <li class="item">
+        <img src="assets/img/post_img_2.png" alt=""><br>
+        <time>2018-05-19</time>
+        <p>あのネオンはいつ交換するのか！？<br>
+        観覧車の謎に迫る！</p>
+        <div class="read_more">READ MORE
+            <p></p>
+        </div>
+    </li>
+    <li class="item">
+        <img src="assets/img/post_img_3.png" alt=""><br>
+        <time>2018-05-18</time>
+        <p>ラソナの社内はこんなのよ</p>
+        <div class="read_more">READ MORE
+            <p></p>
+        </div>
+    </li>
+    <li class="item">
+        <img src="assets/img/post_img_4.png" alt=""><br>
+        <time>2018-05-17</time>
+        <p>お隣のアラハはハワイ？</p>
+        <div class="read_more">READ MORE
+            <p></p>
+        </div>
+    </li>
+    <li class="item">
+        <img src="assets/img/post_img_5.png" alt=""><br>
+        <time>2018-05-16</time>
+        <p>なぜテント？ラソナの人に聞いてみた</p>
+        <div class="read_more">READ MORE
+            <p></p>
+        </div>
+    </li>
+    <li class="item">
+        <img src="assets/img/post_img_6.png" alt=""><br>
+        <time>2015-05-15</time>
+        <p>ベイエリアおしゃれすぎる問題</p>
+        <div class="read_more">READ MORE
+            <p></p>
+        </div>
+    </li>
+</ul>
+</div>
+  <!-- Erina blog -->
 </body>
 </html>
