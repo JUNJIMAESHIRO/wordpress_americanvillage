1a3d4e8425d440ae9f26fea1a2064085111a366f
DESKTOP-NET3VUU\mm_an <eri_matsuo@icloud.com>
2021-12-08 19:14:01 +0900

    メディア対応

--- Erina/index.php
+++ Erina/index.php
@@ -16,38 +16,50 @@
         <img src="assets/img/post_img_1.png" alt=""><br>
         <time>2018-05-20</time>
         <p>おしゃれカフェがありますよ</p>
-        <div class="read_more">READ MORE</div>
+        <div class="read_more">READ MORE
+            <p></p>
+        </div>
     </li>
     <li class="item">
         <img src="assets/img/post_img_2.png" alt=""><br>
         <time>2018-05-19</time>
         <p>あのネオンはいつ交換するのか！？<br>
         観覧車の謎に迫る！</p>
-        <div class="read_more">READ MORE</div>
+        <div class="read_more">READ MORE
+            <p></p>
+        </div>
     </li>
     <li class="item">
         <img src="assets/img/post_img_3.png" alt=""><br>
         <time>2018-05-18</time>
         <p>ラソナの社内はこんなのよ</p>
-        <div class="read_more">READ MORE</div>
+        <div class="read_more">READ MORE
+            <p></p>
+        </div>
     </li>
     <li class="item">
         <img src="assets/img/post_img_4.png" alt=""><br>
         <time>2018-05-17</time>
         <p>お隣のアラハはハワイ？</p>
-        <div class="read_more">READ MORE</div>
+        <div class="read_more">READ MORE
+            <p></p>
+        </div>
     </li>
     <li class="item">
         <img src="assets/img/post_img_5.png" alt=""><br>
         <time>2018-05-16</time>
         <p>なぜテント？ラソナの人に聞いてみた</p>
-        <div class="read_more">READ MORE</div>
+        <div class="read_more">READ MORE
+            <p></p>
+        </div>
     </li>
     <li class="item">
         <img src="assets/img/post_img_6.png" alt=""><br>
         <time>2015-05-15</time>
         <p>ベイエリアおしゃれすぎる問題</p>
-        <div class="read_more">READ MORE</div>
+        <div class="read_more">READ MORE
+            <p></p>
+        </div>
     </li>
 </ul>
 </div>
