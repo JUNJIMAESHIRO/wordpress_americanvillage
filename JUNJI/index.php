8c3e63d94429f96ad40ca6a1f83fe9e489ec1db0
MaeshiroJunji <junjimaeshiro0613@gmail.com>
2021-12-08 18:41:42 +0900

    htmlに書いたコードをphpに貼り付け

--- JUNJI/index.php
+++ JUNJI/index.php
@@ -1,23 +1,24 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="ja">
 <head>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="./assets/css/style.css">
-  <title>Document</title>
+  <title>test</title>
 </head>
 <body>
-  <div class="mian_visual_guideline">
+  <div class="main_visual_guideline">
     <div class="main_visual">
-      <img src="" alt="AmericanVillageの写真">
+      <img src="./assets/img/visual_1_pc.png" alt="AmericanVillageの写真">
       <div class="main_sub_box1">
-
+        <img src="./assets/img/logo.png" alt="">
       </div>
       <div class="main_sub_box2">
-        
+        <img src="./assets/img/visual_text_pc.png" alt="">
       </div>
     </div>
-  </div>  
+    <p>進化し続ける「街」アメリカンビレッジマガジン</p>
+  </div>
 </body>
 </html>
