665ab3cb221dd2ef2423c231a31faa316e41ba1c
DESKTOP-NET3VUU\mm_an <eri_matsuo@icloud.com>
2021-12-09 19:39:37 +0900

    ブログできたよー

--- index.php
+++ index.php
@@ -24,56 +24,22 @@
 <div class="content">
   <h2>Latest Article</h2>
   <ul class="container">
-    <li class="item">
-      <img src="<?php echo get_template_directory_uri(); ?>/assets/img/post_img_1.png" alt=""><br>
-      <time>2018-05-20</time>
-      <p>おしゃれカフェがありますよ</p>
-      <div class="read_more">READ MORE
-        <p></p>
-      </div>
-    </li>
-    <li class="item">
-      <img src="<?php echo get_template_directory_uri(); ?>/assets/img/post_img_2.png" alt=""><br>
-      <time>2018-05-19</time>
-      <p>あのネオンはいつ交換するのか！？<br>
-        観覧車の謎に迫る！</p>
-      <div class="read_more">READ MORE
-        <p></p>
-      </div>
-    </li>
-    <li class="item">
-      <img src="<?php echo get_template_directory_uri(); ?>/assets/img/post_img_3.png" alt=""><br>
-      <time>2018-05-18</time>
-      <p>ラソナの社内はこんなのよ</p>
-      <div class="read_more">READ MORE
-        <p></p>
-      </div>
-    </li>
-    <li class="item">
-      <img src="<?php echo get_template_directory_uri(); ?>/assets/img/post_img_4.png" alt=""><br>
-      <time>2018-05-17</time>
-      <p>お隣のアラハはハワイ？</p>
-      <div class="read_more">READ MORE
-        <p></p>
-      </div>
-    </li>
-    <li class="item">
-      <img src="<?php echo get_template_directory_uri(); ?>/assets/img/post_img_5.png" alt=""><br>
-      <time>2018-05-16</time>
-      <p>なぜテント？ラソナの人に聞いてみた</p>
-      <div class="read_more">READ MORE
-        <p></p>
-      </div>
-    </li>
-    <li class="item">
-      <img src="<?php echo get_template_directory_uri(); ?>/assets/img/post_img_6.png" alt=""><br>
-      <time>2015-05-15</time>
-      <p>ベイエリアおしゃれすぎる問題</p>
-      <div class="read_more">READ MORE
-        <p></p>
-      </div>
-    </li>
-  </ul>
+    <?php
+    if (have_posts()) :
+      while (have_posts()) :
+        the_post(); ?>
+        <li class="item">
+          <?php the_post_thumbnail(); ?>
+          <date><?php echo get_the_date(); ?></date>
+          <p><?php the_title(); ?></p>
+          <div class="read_more">READ MORE</div>
+        </li>
+
+<?php endwhile;
+    else : ?>
+<p>表示する記事がありません</p>
+<?php endif; ?>
+</ul>
 </div>
 <!-- Erina blog -->
 
