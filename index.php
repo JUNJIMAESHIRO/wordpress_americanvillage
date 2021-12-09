39e848d9ba3d32bfeb21fc3b8cb5a9db17b0ceea
MaeshiroJunji <junjimaeshiro0613@gmail.com>
2021-12-09 18:11:46 +0900

    no message

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
-  <!-- Tomori header -->
-<?php get_header();?>
-  <!-- tomori headerここまで -->
-  <!-- Junji main_visual -->
+<!-- Tomori header -->
+  <?php get_header();?>
+<!-- tomori headerここまで -->
+<!-- Junji main_visual -->
   <div class="main_visual_guideline">
     <div class="main_visual">
       <div class="main_sub_box1">
@@ -17,63 +17,63 @@
     </div>
     <p class="main_textcontent">進化し続ける「街」<br class="sp_br">アメリカンビレッジマガジン</p>
   </div>
-  <!-- Junji main_visualここまで -->
-    <!-- Erina blog -->
-  <div class="content">
-    <h2>Latest Article</h2>
-    <ul class="container">
-      <li class="item">
-          <img src="assets/img/post_img_1.png" alt=""><br>
-          <time>2018-05-20</time>
-          <p>おしゃれカフェがありますよ</p>
-          <div class="read_more">READ MORE
-              <p></p>
-          </div>
-      </li>
-      <li class="item">
-          <img src="assets/img/post_img_2.png" alt=""><br>
-          <time>2018-05-19</time>
-          <p>あのネオンはいつ交換するのか！？<br>
-          観覧車の謎に迫る！</p>
-          <div class="read_more">READ MORE
-              <p></p>
-          </div>
-      </li>
-      <li class="item">
-          <img src="assets/img/post_img_3.png" alt=""><br>
-          <time>2018-05-18</time>
-          <p>ラソナの社内はこんなのよ</p>
-          <div class="read_more">READ MORE
-              <p></p>
-          </div>
-      </li>
-      <li class="item">
-          <img src="assets/img/post_img_4.png" alt=""><br>
-          <time>2018-05-17</time>
-          <p>お隣のアラハはハワイ？</p>
-          <div class="read_more">READ MORE
-              <p></p>
-          </div>
-      </li>
-      <li class="item">
-          <img src="assets/img/post_img_5.png" alt=""><br>
-          <time>2018-05-16</time>
-          <p>なぜテント？ラソナの人に聞いてみた</p>
-          <div class="read_more">READ MORE
-              <p></p>
-          </div>
-      </li>
-      <li class="item">
-          <img src="assets/img/post_img_6.png" alt=""><br>
-          <time>2015-05-15</time>
-          <p>ベイエリアおしゃれすぎる問題</p>
-          <div class="read_more">READ MORE
-              <p></p>
-          </div>
-      </li>
-    </ul>
+<!-- Junji main_visualここまで -->
+<!-- Erina blog -->
+<div class="content">
+  <h2>Latest Article</h2>
+  <ul class="container">
+    <li class="item">
+        <img src="assets/img/post_img_1.png" alt=""><br>
+        <time>2018-05-20</time>
+        <p>おしゃれカフェがありますよ</p>
+        <div class="read_more">READ MORE
+            <p></p>
+        </div>
+    </li>
+    <li class="item">
+        <img src="assets/img/post_img_2.png" alt=""><br>
+        <time>2018-05-19</time>
+        <p>あのネオンはいつ交換するのか！？<br>
+        観覧車の謎に迫る！</p>
+        <div class="read_more">READ MORE
+            <p></p>
+        </div>
+    </li>
+    <li class="item">
+        <img src="assets/img/post_img_3.png" alt=""><br>
+        <time>2018-05-18</time>
+        <p>ラソナの社内はこんなのよ</p>
+        <div class="read_more">READ MORE
+            <p></p>
+        </div>
+    </li>
+    <li class="item">
+        <img src="assets/img/post_img_4.png" alt=""><br>
+        <time>2018-05-17</time>
+        <p>お隣のアラハはハワイ？</p>
+        <div class="read_more">READ MORE
+            <p></p>
+        </div>
+    </li>
+    <li class="item">
+        <img src="assets/img/post_img_5.png" alt=""><br>
+        <time>2018-05-16</time>
+        <p>なぜテント？ラソナの人に聞いてみた</p>
+        <div class="read_more">READ MORE
+            <p></p>
+        </div>
+    </li>
+    <li class="item">
+        <img src="assets/img/post_img_6.png" alt=""><br>
+        <time>2015-05-15</time>
+        <p>ベイエリアおしゃれすぎる問題</p>
+        <div class="read_more">READ MORE
+            <p></p>
+        </div>
+    </li>
+  </ul>
   </div>
   <!-- Erina blog -->
 
   <!-- Tomori footer -->
-<?php get_footer(); ?>
+  <?php get_footer(); ?>
