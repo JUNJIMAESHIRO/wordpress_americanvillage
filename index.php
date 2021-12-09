1deaa500a7d25d59035a8fbf51eb1c1d9203d691
DESKTOP-NET3VUU\mm_an <eri_matsuo@icloud.com>
2021-12-09 18:28:03 +0900

    ワードプレステーマ化

--- index.php
+++ index.php
@@ -5,14 +5,14 @@
 <div class="main_visual_guideline">
   <div class="main_visual">
     <div class="main_sub_box1">
-      <img src="./assets/img/logo.png" alt="">
+      <img src="assets/img/logo.png" alt="">
     </div>
     <picture>
       <source media="(max-width: 767px)" srcset="./assets/img/visual_1_sp.png">
-      <img src="./assets/img/visual_1_pc.png" alt="AmericanVillageの写真" class="main_img">
+      <img src="assets/img/visual_1_pc.png" alt="AmericanVillageの写真" class="main_img">
     </picture>
     <div class="main_sub_box2">
-      <img src="./assets/img/visual_text_pc.png" alt="">
+      <img src="assets/img/visual_text_pc.png" alt="">
     </div>
   </div>
   <p class="main_textcontent">進化し続ける「街」<br class="sp_br">アメリカンビレッジマガジン</p>
